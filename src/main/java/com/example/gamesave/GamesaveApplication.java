fce8e1a63f58dca3a3794d35319ac7278cd7e277
CalvinPierce <calvinpierce99@gmail.com>
2022-01-17 14:51:37 -0500

    Initial commit

---
+++ src/main/java/com/example/gamesave/GamesaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamesave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesaveApplication.class, args);
+    }
+
+}
